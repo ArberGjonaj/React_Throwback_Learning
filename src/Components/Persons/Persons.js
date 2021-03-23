af94d70d60e5cd2cc777a1b333f3f15288a774d7
ArberGjonaj <agjonaj@ikubinfo.al>
2021-03-23 11:02:48 +0100

    PureComponents instead of shouldComponentUpdate

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import Person from './Person/Person';
+import React, {PureComponent} from "react";
+import Person from "./Person/Person";
 
-class persons extends React.Component {
-  shouldComponentUpdate(nextProps, nextState){
-    console.log('[Persons.js] shouldComponentUpdate');
-    if(nextProps.persons !== this.props.persons){
-      return true;
-    }else return false;
+class persons extends PureComponent {
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("[Persons.js] shouldComponentUpdate");
+  //   if (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.clicked !== this.props.clicked
+  //   ) {
+  //     return true;
+  //   } else return false;
+  // }
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          name={person.name}
+          key={person.id}
+          age={person.age}
+          withClick={this.props.clicked.bind(this, index)}
+          changed={(event) => this.props.changed(event, person.id)}
+        />
+      );
+    });
   }
-  render(){
-  return this.props.persons.map((person, index) =>{
-    return (
-      <Person
-        name={person.name}
-        key={person.id}
-        age={person.age}
-        withClick={this.props.clicked.bind(this, index)}
-        changed={(event) => this.props.changed(event, person.id)}
-      />
-    );
-  });
-};
-};
+}
 export default persons;
