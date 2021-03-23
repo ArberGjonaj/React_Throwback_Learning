04ae0275bec10d718504653604478f79ac0b2017
ArberGjonaj <agjonaj@ikubinfo.al>
2021-03-23 10:44:38 +0100

    Using shouldComponentUpdate for Optimization

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -5,12 +5,11 @@ const Cockpit = (props) => {
   useEffect(
     ()=>{
     console.log('[Cockpit.js] useEffect ')
-    const timer = setTimeout(()=>{
+    setTimeout(()=>{
       alert('Saved data to cloud')
     },1000);
     return () => {
       
-      clearTimeout(timer)
       console.log('[Cockpit.js] Cleanup work in useEffect')
     }
   }, [])
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-  return props.persons.map((person, index) =>{
+class persons extends React.Component {
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js] shouldComponentUpdate');
+    if(nextProps.persons !== this.props.persons){
+      return true;
+    }else return false;
+  }
+  render(){
+  return this.props.persons.map((person, index) =>{
     return (
       <Person
         name={person.name}
         key={person.id}
         age={person.age}
-        withClick={props.clicked.bind(this, index)}
-        changed={(event) => props.changed(event, person.id)}
+        withClick={this.props.clicked.bind(this, index)}
+        changed={(event) => this.props.changed(event, person.id)}
       />
     );
   });
+};
 };
 export default persons;
