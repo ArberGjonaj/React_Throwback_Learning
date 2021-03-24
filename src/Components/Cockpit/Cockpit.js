2812e92f89d9a939d5e329c90d27b619157132ba
ArberGjonaj <agjonaj@ikubinfo.al>
2021-03-24 12:23:47 +0100

    Refs with React Hooks

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,25 +1,26 @@
-import React, {useEffect} from 'react';
-import classes from './Cockpit.module.css';
+import React, { useEffect, useRef } from "react";
+import classes from "./Cockpit.module.css";
 
 const Cockpit = (props) => {
-  useEffect(
-    ()=>{
-    console.log('[Cockpit.js] useEffect ')
-    setTimeout(()=>{
-      alert('Saved data to cloud')
-    },1000);
+  const toggleBtnRef = useRef(null);
+
+  useEffect(() => {
+    // console.log("[Cockpit.js] useEffect ");
+    // setTimeout(() => {
+    //   alert("Saved data to cloud");
+    // }, 1000);
+    toggleBtnRef.current.click();
     return () => {
-      
-      console.log('[Cockpit.js] Cleanup work in useEffect')
-    }
-  }, [])
-  useEffect(()=>{
-   
-    console.log('[Cockpit.js] useEffect second')
-    return()=>{
-      console.log('[Cockpit.js] Cleanup work in useEffect')
-    }
-  })
+      console.log("[Cockpit.js] Cleanup work in useEffect");
+    };
+  }, []);
+  
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect second");
+    return () => {
+      console.log("[Cockpit.js] Cleanup work in useEffect");
+    };
+  });
   let buttonClasses = [classes.Button];
   const joinClasses = [];
   if (props.showPerson) {
@@ -32,12 +33,16 @@ const Cockpit = (props) => {
     joinClasses.push(classes.Bold); //  classes = ['red'], ['bold'];
   }
 
-  buttonClasses = buttonClasses.join(' ');
+  buttonClasses = buttonClasses.join(" ");
   return (
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
-      <p className={joinClasses.join(' ')}>This is Really Working!</p>
-      <button className={buttonClasses} onClick={props.clicked}>
+      <p className={joinClasses.join(" ")}>This is Really Working!</p>
+      <button
+        ref={toggleBtnRef}
+        className={buttonClasses}
+        onClick={props.clicked}
+      >
         Toggle Person!
       </button>
     </div>
